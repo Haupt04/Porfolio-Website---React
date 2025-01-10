b35160e45a8f5728ea3910673a6b39a5cbead6f9
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-10 15:03:00 +0200

    Completed the CSS for the Nav Component

--- porfolio-website/src/Pages/Home/HeroSection.jsx
+++ porfolio-website/src/Pages/Home/HeroSection.jsx
@@ -1,9 +1,9 @@
 export default function HeroSection() {
     return (
         <section id="heroSection" className="hero--section">
-            <div className="hero--section--content--box">
+            <div className="hero--section--content-box">
                 <div className="hero--section--content">
-                    <p className="section-title">Hey, I'm Brittany</p>
+                    <p className="section--title">Hi, I'm Brittany</p>
                     <h1 className="hero--section--title">
                         <span className="hero--section--title--color">Full Stack</span>{" "}
                         <br />
