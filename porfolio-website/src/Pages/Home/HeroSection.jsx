cfddd70c38328dab38e9057eaa5ce57dd7068cb0
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-10 13:32:05 +0200

    Completed CSS for Hero Section

--- porfolio-website/src/Pages/Home/HeroSection.jsx
+++ porfolio-website/src/Pages/Home/HeroSection.jsx
@@ -1,11 +1,11 @@
 export default function HeroSection() {
     return (
         <section id="heroSection" className="hero--section">
-            <div className="hero--section--content-box">
-                <div className="hero--section-content">
+            <div className="hero--section--content--box">
+                <div className="hero--section--content">
                     <p className="section-title">Hey, I'm Brittany</p>
                     <h1 className="hero--section--title">
-                        <span className="hero-section-title--color">Full Stack</span>{" "}
+                        <span className="hero--section--title--color">Full Stack</span>{" "}
                         <br />
                         Developer
                     </h1>
