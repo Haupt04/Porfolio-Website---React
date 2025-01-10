2e550e69e2d211a75d624005ec1209f4dd28e1a4
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-10 17:53:14 +0200

    Completed About Me Component

---
+++ porfolio-website/src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,18 @@
+
+export default function AboutMe () {
+    return (
+        <section className="about--section" id="AboutMe">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills-section-heading">About Me</h1>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio cum animi aperiam debitis magnam itaque iure eligendi perspiciatis amet! Explicabo odit ipsa modi eos! Amet adipisci facilis numquam velit esse?</p>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio cum animi aperiam debitis magnam itaque iure eligendi perspiciatis amet! Explicabo odit ipsa modi eos! Amet adipisci facilis numquam velit esse?</p>
+                </div>
+            </div>
+        </section>
+    )
+}
