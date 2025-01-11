5f817ef46f4da3b13e9d3caced45ae2bb32a51ad
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-11 10:54:52 +0200

    Completed CSS for MyPortfolio Section

--- porfolio-website/src/Pages/Home/MyPorfolio.jsx
+++ porfolio-website/src/Pages/Home/MyPorfolio.jsx
@@ -4,7 +4,7 @@ export default function MyPortolio() {
     return (
         <section className="portfolio--section" id="MyPortfolio">
             <div className="portfolio--container-box">
-                <div className="portfolio-container">
+                <div className="portfolio--container">
                     <p className="sub--title">Recent Projects</p>
                     <h2 className="section--heading">My Portfolio</h2>
                 </div>
@@ -39,7 +39,24 @@ export default function MyPortolio() {
                                 <h3 className="portfolio--section--title">{item.title}</h3>
                                 <p className="text-md">{item.description}</p>
                             </div>
-                            <p className="text-sm portfolio--link"></p>
+                            <p className="text-sm portfolio--link">
+                                {item.link}
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                viewBox="0 0 20 19"
+                                fill="none"
+                                >
+                                <path
+                                    d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
+                                    stroke="currentColor"
+                                    stroke-width="2.66667"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                />
+                                </svg>
+                            </p>
                         </div>
 
                     </div>
