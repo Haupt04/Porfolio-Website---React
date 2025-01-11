cb0ad31c6413fc595cbd5cda8d9fbd4e1439882e
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-01-11 11:20:58 +0200

    Completed Testimonial Component HTML

--- porfolio-website/src/Pages/Home/Homescreen/index.jsx
+++ porfolio-website/src/Pages/Home/Homescreen/index.jsx
@@ -2,6 +2,7 @@ import AboutMe from "../AboutMe";
 import HeroSection from "../HeroSection";
 import MySkills from "../MySkills";
 import MyPortolio from "../MyPorfolio";
+import Testimonial from "../Testimonials";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
             <MySkills />
             <AboutMe />
             <MyPortolio />
+            <Testimonial />
         </>
     )
 }
